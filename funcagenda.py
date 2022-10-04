c1f34d0e52ebd0054c2855852d9dd220bb6096f2
Pablo Moore <pablomoore96@gmail.com>
2022-10-04 14:47:50 -0300

    agenda hecha en python

---
+++ funcagenda.py
@@ -0,0 +1,44 @@
+# Proponer una solucion que simule una agenda de contactos haciendo uso de diccionarios.
+# Para el diccionario las llaves seran los nombres de los contactos y como valor estará una tupla que contenga el número telefónico y el correo electrónico.
+# Se tendrá un menú con las siguientes opciones: 1 Agregar contacto, 2 mostrar contacto, 3 buscar contacto, 4 eliminar contacto, 5 salir.
+# Su propuesta deberá utilizar minimamente: Constantes y/o variables, cond selectivos, multiples, bucles, diccionarios y tuplas, uso de funciones y main.
+
+
+
+agenda = {}
+
+def add_contact():
+    # Agregar un contacto a la agenda
+    nombre=input("Ingrese nombre de contacto: \n")
+    tel=input("Ingrese su número de teléfono: \n")
+    while not tel.isnumeric():
+        tel=input("Ingrese su número de teléfono correctamente: \n")
+    tel=int(tel)
+    correo=input("Ingrese su email: \n")
+    agenda.update({nombre: (tel, correo)})
+
+    return agenda
+
+
+def show_contact():
+
+    for key, values in agenda.items():
+        print(f"Nombre: {key} \nTelefono: {values[0]} \nEmail: {values[1]}")
+        print("")
+    # Mostrar todos los contactos en la agenda   
+
+def search_contact():
+    nom=input("Ingresar nombre a buscar: \n") #Declaro esta variable para utilizarla como una referencia a la key del diccionario.
+    for key, values in agenda.items():
+        if (nom == key): #Si la variale nom es igual al valor de una, kay, la mostrará en pantalla.
+            print(f"Nombre: {key} \nTelefono: {values[0]} \nEmail: {values[1]}")
+            print("")
+
+def delete_contact():
+    for key, values in agenda.items():
+        print(f"Nombre: {key} \nTelefono: {values[0]} \nEmail: {values[1]}")
+        print("")
+    nom=input("Ingresar contacto a eliminar: \n") #Usé la misma base que en mostrar contacto, pero acá declare la variable nom, y en este caso
+    if (nom == key):                              # la condicioné para que si es igual a key, elimine el string de "nom"
+        agenda.pop(nom)
+
---
+++ main.py
@@ -0,0 +1,21 @@
+import funcagenda
+
+if __name__=="__main__": #Acá para dar uso al main, llamé a la función desde el otro archivo, y armé el menú  usando un while, que corra el programa
+    option_menu = 0      # mientras que el valor de la opción sea diferente de 5
+    while(option_menu != 5):
+        print('\n1 Agregar contacto \n2 mostrar contacto \n3 buscar contacto \n4 eliminar contacto \n5 salir\n')
+        op = input("Seleccionar opcion del menu \n")
+        if (op.isdigit()): #Aca cree una excepción, similar a las que suelo hacer para que no rompa el programa si la opción introducida no es un número
+            option_menu = int(op) #Acá le pedi al usuario que seleccióne la opción que necesite, y cada opción corre una función
+            if(option_menu == 1):
+                funcagenda.add_contact()
+            elif(option_menu == 2):
+                funcagenda.show_contact()
+            elif(option_menu == 3):
+                funcagenda.search_contact()
+            elif(option_menu == 4):
+                funcagenda.delete_contact()
+            elif(option_menu > 5 or option_menu < 1): #Esta opción está por si el usuario pone una opción numérica distinta de las solicitadas en el menú
+                print("Elegir una opcion valida")
+            else: #Opción == 5, rompe el bucle
+                print("Gracias por usar el programa.")
