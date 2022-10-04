c340c71a04aaf29a7e3a08643d4a96c716d7417c
Pablo Moore <105167808+PabloMoore96@users.noreply.github.com>
2022-10-04 17:30:50 -0300

    Corrección de texto en línea 6

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import funcagenda
 if __name__=="__main__": #Acá para dar uso al main, llamé a la función desde el otro archivo, y armé el menú  usando un while, que corra el programa
     option_menu = 0      # mientras que el valor de la opción sea diferente de 5
     while(option_menu != 5):
-        print('\n1 Agregar contacto \n2 Mostrar contacto \n3 Buscar contacto \n4 Eliminar contacto \n5 Salir\n')
+        print('\n1 Agregar contacto \n2 Mostrar contactos \n3 Buscar contacto \n4 Eliminar contacto \n5 Salir\n')
         op = input("Seleccionar opcion del menu \n")
         if (op.isdigit()): #Aca cree una excepción, similar a las que suelo hacer para que no rompa el programa si la opción introducida no es un número
             option_menu = int(op) #Acá le pedi al usuario que seleccióne la opción que necesite, y cada opción corre una función
